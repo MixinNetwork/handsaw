6c55d137d406248b8cb253f75654af67bf51f5bb
YeungKC <I@YeungKC.com>
2022-05-24 14:47:12 +0800

    support flutter

--- src/main/kotlin/one/mixin/handsaw/generate.kt
+++ src/main/kotlin/one/mixin/handsaw/generate.kt
@@ -1,5 +1,6 @@
 package one.mixin.handsaw
 
+import net.pearx.kasechange.toCamelCase
 import java.io.File
 
 interface Generator {
@@ -8,7 +9,7 @@ interface Generator {
 }
 
 val androidPlaceHolder = "%[\\d]+[\$][d|s]".toRegex()
-val iosPlaceHolder = "%[\\d]+[\$][@]".toRegex()
+val iosPlaceHolder = "%@".toRegex()
 
 class AndroidGenerator : Generator {
   override fun generate(parseResult: ParseResult, outputFile: String?) {
@@ -17,7 +18,8 @@ class AndroidGenerator : Generator {
     } else {
       outputFile
     }
-    val outDir = File("$path${File.separator}output${File.separator}Android")
+    val outDir =
+      File("$path${File.separator}output${File.separator}Android")
     if (outDir.exists()) {
       outDir.delete()
     }
@@ -86,7 +88,10 @@ class AndroidGenerator : Generator {
       } else if (k.endsWith(".count")) {
         val localPluralKey = k.substringBeforeLast(".count")
         val singleExists = data[localPluralKey]
-        if (singleExists != null && singleExists.isNotEmpty() && singleExists.getOrNull(index)?.isNotBlank() == true) {
+        if (singleExists != null && singleExists.isNotEmpty() && singleExists.getOrNull(
+            index
+          )?.isNotBlank() == true
+        ) {
           result.append("\t<plurals name=\"$localPluralKey\">\n")
             .append("\t\t<item quantity=\"other\">$value</item>\n")
           pluralKey = localPluralKey
@@ -106,9 +111,8 @@ class AndroidGenerator : Generator {
     return "<resources xmlns:tools=\"http://schemas.android.com/tools\">\n$result</resources>"
   }
 
-  override fun validPlatform(platform: String): Boolean {
-    return platform.equals("Android", true) || platform.equals("Mobile", true)
-  }
+  override fun validPlatform(platform: String): Boolean =
+    platform.contains("android", true)
 }
 
 class IOSGenerator(
@@ -126,7 +130,8 @@ class IOSGenerator(
     }
     outDir.mkdir()
 
-    val keyEnValMap = if (keyType == KeyType.EnValue) mutableMapOf<String, String>() else null
+    val keyEnValMap =
+      if (keyType == KeyType.EnValue) mutableMapOf<String, String>() else null
     parseResult.langList.forEach { lang ->
       val text = convertLang(lang, parseResult, keyEnValMap)
       val dirName = "$lang.lproj"
@@ -149,7 +154,11 @@ class IOSGenerator(
     }
   }
 
-  private fun convertLang(lang: String, parseResult: ParseResult, keyEnValMap: MutableMap<String, String>?): String {
+  private fun convertLang(
+    lang: String,
+    parseResult: ParseResult,
+    keyEnValMap: MutableMap<String, String>?
+  ): String {
     val index = parseResult.langList.indexOf(lang)
     val data = parseResult.dataList
     val platformMap = parseResult.platformMap
@@ -208,9 +217,97 @@ class IOSGenerator(
     return result.toString()
   }
 
-  override fun validPlatform(platform: String): Boolean {
-    return platform.equals("iOS", true) || platform.equals("Mobile", true)
+  override fun validPlatform(platform: String): Boolean =
+    platform.contains("ios", true)
+}
+
+class FlutterGenerator : Generator {
+  override fun generate(parseResult: ParseResult, outputFile: String?) {
+    val validDataList = parseResult.dataList.filter { it ->
+      parseResult.platformMap[it.key].let {
+        it != null && validPlatform(it)
+      }
+    }
+
+    val langData = parseResult.langList.groupBy({ it }, { lang ->
+      validDataList.map {
+        it.key to it.value[parseResult.langList.indexOf(lang)]
+      }.toMap()
+    }).map {
+      it.key to it.value.first()
+    }.toMap()
+
+    val path = if (outputFile.isNullOrBlank()) {
+      System.getProperty("user.dir")
+    } else {
+      outputFile
+    }
+
+    val outDir =
+      File("$path${File.separator}output${File.separator}Flutter")
+    if (outDir.exists()) {
+      outDir.delete()
+    }
+    outDir.mkdir()
+
+    langData.forEach { lang ->
+      val file =
+        File("${outDir.absolutePath}${File.separator}intl_${lang.key}.arb")
+      if (file.exists()) {
+        file.delete()
+      }
+
+      file.appendText("{")
+
+      file.appendText("\n")
+
+      val originalMap = lang.value
+
+      val newMap = mutableMapOf<String, String>()
+
+      originalMap.forEach {
+        val value = it.value.let { _value ->
+          var value = _value
+          (androidPlaceHolder.findAll(value) + iosPlaceHolder.findAll(value)).forEachIndexed { index, matchResult ->
+            value = value.replace(matchResult.value, "{arg$index}")
+          }
+          value.replace("\n", "\n")
+
+          value.trim()
+        }
+
+        if (value.isBlank()) return@forEach
+
+        if (it.key.endsWith(".count")) {
+          val key = it.key.substringBeforeLast(".count").toCamelCase()
+          val one = newMap[key]
+
+          newMap[key] = "{count, plural, one{${one}} other{${value}}}"
+          return@forEach
+        }
+
+        val key = it.key.toCamelCase().let { key ->
+          if ("continue" == key) return@let "${key}Text"
+          key
+        }
+        newMap[key] = value
+
+      }
+
+      newMap.toSortedMap().map {
+        "\"${it.key}\" : \"${it.value}\""
+      }.joinToString(separator = ",\n").let {
+        file.appendText(it)
+      }
+
+      file.appendText("\n")
+      file.appendText("}")
+    }
   }
+
+  override fun validPlatform(platform: String): Boolean =
+    platform.contains("Desktop", true)
+
 }
 
 enum class KeyType {
